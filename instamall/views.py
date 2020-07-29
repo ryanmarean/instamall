d42b99636985647f1c17eb6e8c40d31faa8d0520
Ryan M <ryan.marean@gmail.com>
2020-07-29 15:59:15 -0700

    all test data entered

--- instamall/views.py
+++ instamall/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
-def index(render):
-    return render(request, "homepage")
-    
-def roy_mall(request):
-    return redirect('/roy_store')
 
-#this will be function for Roys stores
-def roy_store(request):
-    pass
+def index(render):
+    return render(request, 'mall_lane.html')
+    
+def show_mall(request, mall_id):
+    return render(request, 'show_mall.html')
+
+def show_store(request, store_id):
+    return render(request, 'show_store.html')
+
+def shopping_cart(request):
+    return render(request, 'shopping_cart.html')
+
+def purchase_complete(request):
+    return render(request, 'purchase_complete.html')
